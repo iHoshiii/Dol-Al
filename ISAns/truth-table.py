0edaf573f57083303179c1061b909dbb33de94b2
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-03-13 08:56:12 +0800

    Ans IS

--- ISAns/truth-table.py
+++ ISAns/truth-table.py
@@ -1,28 +1,35 @@
 import itertools
 
-n = int(input("No. of Variable: "))
-expr = input("Find: ").lower()
+n = int(input("Number of Variables: "))
+expr = input("Find: ").lower().replace(" ", "")
 
 variables = [chr(ord('p') + i) for i in range(n)]
 
 print("\nOutput:")
 
 # header
-print(" ".join(v.upper() for v in variables), expr.replace(" ", "").upper())
+print(" ".join(v.upper() for v in variables), expr.upper())
 
-for row in itertools.product(['T','F'], repeat=n):
-
+for row in itertools.product(['T', 'F'], repeat=n):
     values = dict(zip(variables, row))
-
-    p = values.get('p','F')
-    q = values.get('q','F')
-
+    
+    # helper function to evaluate a variable or its negation
+    def eval_var(term):
+        if term.startswith('-') or term.startswith('¬'):
+            var = term[1:]
+            return 'F' if values.get(var, 'F') == 'T' else 'T'
+        else:
+            return values.get(term, 'F')
+    
     result = 'F'
-
-    if expr == "p v q":
-        result = 'T' if p == 'T' or q == 'T' else 'F'
-
-    elif expr == "p ^ q":
-        result = 'T' if p == 'T' and q == 'T' else 'F'
-
+    
+    if expr in ["p^q", "p&q"]:
+        result = 'T' if eval_var('p') == 'T' and eval_var('q') == 'T' else 'F'
+    elif expr in ["pvq", "p|q"]:
+        result = 'T' if eval_var('p') == 'T' or eval_var('q') == 'T' else 'F'
+    elif expr.startswith('-') or expr.startswith('¬'):
+        result = eval_var(expr)
+    else:
+        result = eval_var(expr)
+    
     print(" ".join(row), result)
