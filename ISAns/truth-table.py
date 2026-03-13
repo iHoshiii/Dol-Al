2beb3e612da605844aee62b07c7c400696629ffe
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-03-13 08:20:14 +0800

    Ans IS

--- ISAns/truth-table.py
+++ ISAns/truth-table.py
@@ -1,26 +1,28 @@
 import itertools
 
-n = int(input("Enter number of variables: "))
+n = int(input("No. of Variable: "))
+expr = input("Find: ").lower()
 
 variables = [chr(ord('p') + i) for i in range(n)]
 
 print("\nOutput:")
 
-# print header with spacing
-header = variables + ["¬P", "P^Q", "PvQ", "P→Q", "P↔Q"]
-print("".join(f"{h:<5}" for h in header))
+# header
+print(" ".join(v.upper() for v in variables), expr.replace(" ", "").upper())
 
 for row in itertools.product(['T','F'], repeat=n):
 
-    p = row[0]
-    q = row[1] if n > 1 else 'F'
+    values = dict(zip(variables, row))
 
-    not_p = 'F' if p == 'T' else 'T'
-    p_and_q = 'T' if p == 'T' and q == 'T' else 'F'
-    p_or_q = 'T' if p == 'T' or q == 'T' else 'F'
-    p_imp_q = 'F' if p == 'T' and q == 'F' else 'T'
-    p_bi_q = 'T' if p == q else 'F'
+    p = values.get('p','F')
+    q = values.get('q','F')
 
-    row_data = list(row) + [not_p, p_and_q, p_or_q, p_imp_q, p_bi_q]
+    result = 'F'
 
-    print("".join(f"{item:<5}" for item in row_data))
+    if expr == "p v q":
+        result = 'T' if p == 'T' or q == 'T' else 'F'
+
+    elif expr == "p ^ q":
+        result = 'T' if p == 'T' and q == 'T' else 'F'
+
+    print(" ".join(row), result)
